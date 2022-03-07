054097afb5b002f90b6e815980ea5ade8fad76fa
Murilo Rocha <murilo.rocha@quintoandar.com.br>
2022-03-07 16:24:38 -0300

    add cookies

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from Autentificao import Autentificao
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 import pandas as pd
@@ -11,8 +12,12 @@ import time
 
 fotos = pd.read_csv("./arquivos/Fotos.csv", header=None, sep=",", dtype=str)
 dados = pd.read_csv("./arquivos/Dados.csv", header=None, sep=",", dtype=str)
-s = Service("/usr/local/bin/chromedriver")
-driver = webdriver.Chrome(service=s)
+
+chrome_options = Options()
+chrome_options.add_argument("--user-data-dir=chrome-data")
+
+s = Service()
+driver = webdriver.Chrome(service=s,options=chrome_options)
 driver.get("https://owner-conversion.quintoandar.com.br/register/new/owner")
 Autentificao.autentificar(driver)
 WebDriverWait(driver, 200).until(
