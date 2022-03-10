426d127a433b229c323ba623d0a7add6b0054d34
Murilo Rocha <murilo.rocha@quintoandar.com.br>
2022-03-09 21:27:07 -0300

    imagiiiiiii ok

--- Foto.py
+++ Foto.py
@@ -2,6 +2,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 from datetime import datetime
 import time
 import urllib.request
@@ -23,7 +24,13 @@ class Foto:
                 id_imovel_imob = dados[1][index]
                 id_imovel = dados[42][index]
                 check_file = "./imag/" + id_imovel
-                print("start imovel " + id_imovel)
+                print(
+                    "start imagem imob ("
+                    + id_imovel_imob
+                    + ") 5A ("
+                    + id_imovel
+                    + ")"
+                )
                 if os.path.isdir(check_file) != True:
                     print("start download_photos")
                     founded = fotos.loc[fotos[0] == id_imovel_imob]
@@ -82,32 +89,113 @@ class Foto:
                                 )
                             )
                         )
+                        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
                         caminho_atual = (
                             os.path.dirname(os.path.realpath(__file__))
                             + "/imag/"
                             + id_imovel
                         )
+
+                        driver.find_element_by_id("input-fotos").send_keys(
+                            caminho_atual + "/1.jpg"
+                        )
+                        WebDriverWait(driver, 10).until(
+                            ec.invisibility_of_element_located(
+                                (By.XPATH, "/html/body/div[15]")
+                            )
+                        )
+
+                        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+                        time.sleep(2)
+                        hoverboy = driver.find_element_by_xpath(
+                            "/html/body/div[8]/div/div/form/div[2]/div[7]/div[6]/div[5]/div/div/div[1]"
+                        )
+                        hov = ActionChains(driver).move_to_element(hoverboy)
+                        hov.perform()
+                        time.sleep(2)
+
+
+                        WebDriverWait(driver, 10).until(
+                            ec.visibility_of_element_located(
+                                (By.XPATH, '//a[@data-original-title="Definir como capa"]')
+                            )
+                        )
+
+                        driver.find_element_by_xpath('//a[@data-original-title="Definir como capa"]').click()
+
+                        WebDriverWait(driver, 10).until(
+                            ec.visibility_of_element_located(
+                                (By.XPATH, "//button[contains(text(), 'Salvar Capa')]")
+                            )
+                        )
+                        time.sleep(4)
+                        driver.find_element_by_xpath("//button[contains(text(), 'Salvar Capa')]").click()
+
                         # time.sleep(10000)
-                        for imagens in os.listdir(caminho_atual):
+                        imagens_restantes = os.listdir(caminho_atual)[0:-1]
+                        print(imagens_restantes)
+                        for imagens in imagens_restantes:
                             print(caminho_atual + "/" + imagens)
                             driver.find_element_by_id("input-fotos").send_keys(
                                 caminho_atual + "/" + imagens
                             )
-                            WebDriverWait(driver, 10).until(
+                            WebDriverWait(driver, 60).until(
                                 ec.invisibility_of_element_located(
                                     (By.XPATH, "/html/body/div[15]")
                                 )
                             )
-                        # time.sleep(10000)
-                        print("aguardando botao salvar")
-                        WebDriverWait(driver, 200).until(
-                            ec.visibility_of_element_located(
-                                (
-                                    By.XPATH,
-                                    '//*[@id="salvar_publicar"]',
-                                )
+                        WebDriverWait(driver, 60).until(
+                            ec.invisibility_of_element_located(
+                                (By.XPATH, "/html/body/div[15]")
                             )
                         )
+
+                        # driver.find_element_by_xpath('/html/body/div[9]/div/div/form/div[2]/div[5]/div/div[2]/div[1]/div[1]/label').click()
+
+                        # NOVA PAG PUBLICAR
+                        driver.get("https://user.quintoandar.com.br/imovel/editar/"+id_imovel+"/fotos")
+
+                        WebDriverWait(driver, 60).until(
+                            ec.visibility_of_element_located(
+                                (By.ID, "btnPublicar")
+                            )
+                        )
+                        driver.find_element_by_id(
+                            'btnPublicar',
+                        ).click()
+
+
+                        WebDriverWait(driver, 60).until(
+                            ec.visibility_of_element_located(
+                                (By.ID, "popUpCallBackPublicar")
+                            )
+                        )
+
+                        # FIM NOVA PAG PUBLICAR
+
+
+                        # time.sleep(10000)
+                        # print("aguardando botao salvar")
+                        # time.sleep(2)
+                        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+                        # time.sleep(2)
+                        # WebDriverWait(driver, 200).until(
+                        #     ec.visibility_of_element_located(
+                        #         (
+                        #             By.XPATH,
+                        #             '//*[@id="salvar_publicar"]',
+                        #         )
+                        #     )
+                        # )
+
+                        # time.sleep(2)
+                        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+                        # time.sleep(2)
+
+                        # driver.find_element_by_xpath(
+                        #     '//*[@id="salvar_publicar"]',
+                        # ).click()
+
                         print(
                             "finish imagem imob ("
                             + id_imovel_imob
@@ -115,9 +203,35 @@ class Foto:
                             + id_imovel
                             + ")"
                         )
-                        driver.find_element_by_xpath(
-                            '//*[@id="salvar_publicar"]',
-                        ).click()
+
+
+                        # try:
+                        #     WebDriverWait(driver, 10).until(
+                        #         ec.visibility_of_element_located(
+                        #             (
+                        #                 By.XPATH,
+                        #                 '//div[@class="errosAdmin"]',
+                        #             )
+                        #         )
+                        #     )
+                        #     dados[45][index] = "error-" + driver.find_element_by_class("errosAdmin").text
+                        #     dados.to_csv(
+                        #         "./arquivos/dados" + number + ".csv",
+                        #         header=None,
+                        #         sep=",",
+                        #         index=False,
+                        #     )
+                        # except TimeoutException:
+
+                        # driver.find_elements_by_class_name("foto-container")
+                        #
+                        # ec.visibility_of_element_located(
+                        #     (
+                        #         By.XPATH,
+                        #         '//div[@class="errosAdmin"]',
+                        #     )
+                        # )
+
                         dados[45][index] = "ok-img ("+datetime.today().strftime("%Y-%m-%d %H:%M:%S")+")"
                         dados.to_csv(
                             "./arquivos/dados" + number + ".csv",
